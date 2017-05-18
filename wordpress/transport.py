fb1ee18ff21fbaba5038f9b8926b19cf058a66ea
webmaster@bcbud.store <webmaster@bcbud.store>
2017-05-18 11:13:42 -0400

    just a test

--- wordpress/transport.py
+++ wordpress/transport.py
@@ -65,11 +65,12 @@ class API_Requests_Wrapper(object):
         args_headers = kwargs.get("headers", False)
         print args_headers
 
-        # if args_headers:
-        #     for k, v in args_headers.iteritems():
-        #
-        #         headers[k] = v
-        #     kwargs. = headers
+        if args_headers:
+            for k, v in args_headers.iteritems():
+
+                headers[k] = v
+
+        kwargs["headers"] = headers
 
         print "HEADERS DICT before: " + str(headers)
 
